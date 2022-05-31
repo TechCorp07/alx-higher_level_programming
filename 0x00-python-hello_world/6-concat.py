d8ed55e607e75e412999431c02a855038af463c1
Einstein <TechCorp07@users.noreply.github.com>
2022-05-31 03:38:00 +0200

    6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = " Holberton School!"
-data = str1+str2
-print(data)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
