c6a3ae2204a0985291eb91b5fe95cfc88520bbef
Einstein <TechCorp07@users.noreply.github.com>
2022-07-06 00:56:20 +0200

    all

--- 0x0B-python-input_output/10-class_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines class_to_json"""
-
-
-def class_to_json(obj):
-    """Returns dictionary description with simple data
-    structure for JSON serialization of object"""
-    return obj.__dict__
