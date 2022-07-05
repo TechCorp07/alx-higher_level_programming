bc61ec2033a6f41c315ad0ffa2cfd026d303a56d
Einstein <TechCorp07@users.noreply.github.com>
2022-07-06 00:50:54 +0200

    Update and rename 3-write_file.py to 3-to_json_string.py

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines write_file"""
-
-
-def write_file(filename="", text=""):
-    """Writes string to text file, returns number of chars written"""
-    with open(filename, 'w') as f:
-        return f.write(text)
