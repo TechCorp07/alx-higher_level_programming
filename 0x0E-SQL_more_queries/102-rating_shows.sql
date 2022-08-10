fefd4cb112ee133d1dd4178a320bc30d744cdf0f
Einstein <TechCorp07@users.noreply.github.com>
2022-08-10 20:54:58 +0200

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,12 +1,8 @@
--- lists all genres the show dexter is listed under
-SELECT tv_shows.title 
+-- lists all shows by their rating
+SELECT tv_shows.title,
+       SUM(tv_show_ratings.rate) AS rating
        FROM tv_shows
-       WHERE tv_shows.title NOT IN (
-       SELECT tv_shows.title 
-       	      FROM tv_shows
-       	      INNER JOIN tv_show_genres
-       	      ON tv_shows.id = tv_show_genres.show_id
-       	      	 INNER JOIN tv_genres
-	  	 ON tv_show_genres.genre_id = tv_genres.id
-	  	 WHERE tv_genres.name = 'Comedy')
-ORDER BY tv_shows.title;
+       JOIN tv_show_ratings
+       ON tv_show_ratings.show_id = tv_shows.id
+       GROUP BY tv_shows.title
+       ORDER BY rating DESC;
