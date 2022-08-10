acdceb0e29696598ea79acb9a4cc5159a1c98eff
Einstein <TechCorp07@users.noreply.github.com>
2022-08-10 20:30:32 +0200

    Create 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Uses the hbtn_0d_tvshows database to lists all shows without the denre Comedy
+SELECT g.title
+FROM tv_genres AS g
+WHERE g.title NOT IN (
+        SELECT g.title
+        FROM tv_genres AS g
+            JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+            JOIN tv_shows AS s ON sg.show_id = s.id
+        WHERE s.title = "Dexter"
+    )
+ORDER BY g.title;
