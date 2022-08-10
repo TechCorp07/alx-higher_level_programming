9be5249edb852b77b58216fac5f8a24da05ca7ef
Einstein <TechCorp07@users.noreply.github.com>
2022-08-10 20:51:04 +0200

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,11 @@
 -- Uses the hbtn_0d_tvshows database to lists all shows without the denre Comedy
-SELECT g.title
-FROM tv_genres AS g
-WHERE g.title NOT IN (
-        SELECT g.title
-        FROM tv_genres AS g
-            JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-            JOIN tv_shows AS s ON sg.show_id = s.id
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+        SELECT tv_shows.title
+        FROM tv_shows
+            JOIN tv_show_genres AS sg ON tv_shows.id = sg.show_id
+            JOIN tv_genres AS s ON sg.genre_id = s.id
         WHERE s.name = "Comedy"
     )
-ORDER BY g.title;
+ORDER BY tv_shows.title;
